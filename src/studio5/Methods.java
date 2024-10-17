fa3bfa442ad8be4a9d7a5b77d5a5a22705764f52
alishachang <alishachang@localhost>
2024-10-17 11:17:46 -0500

    yay!

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -16,7 +18,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
 		return distance;
 	}
 
@@ -34,17 +36,24 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
 		
+		Color blue = new Color (0, 109, 219);
+		StdDraw.setPenColor(blue);
+		StdDraw.filledCircle(x, y, 3.0/4.0*radius);
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
-		
+		Color red = new Color (146, 0, 0);
+		StdDraw.setPenColor(red);
+		StdDraw.filledCircle(x, y, 1.0/2.0*radius);
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
 
+		Color yellow = new Color (255, 255, 109);
+		StdDraw.setPenColor(yellow);
+		StdDraw.filledCircle(x, y, 1.0/4.0*radius);
 		
 	}
 
@@ -62,6 +71,15 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
+		for (int i=0; i<=source.length()-1; i++) {
+			char currentChar = source.charAt(i);
+			if (currentChar==target) {
+				result += replacement;
+				}
+				else {
+					result += currentChar;
+			}
+		}
 		
 		return result;
 	}
@@ -75,7 +93,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i=0; i<values.length; i++) {
+			sum = sum + values[i];
+		}
 		return sum;
 	}
 
@@ -87,9 +107,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int [length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
+		for (int i=0; i<values.length; i++) {
+			values[i]=value;
+		}
 		
 
 		return values;
